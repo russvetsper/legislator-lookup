fb39a5a0482b5e66c9f47c21f075381d8fabee0b
rouz majlessi, russ vetsper and yi chiang <rouz+russ+yi@>
2016-09-06 12:26:45 -0700

    add api to .env

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,8 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.myApiKey = process.env.apiKey;
+
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
